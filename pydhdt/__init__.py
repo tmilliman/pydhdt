29ff25bdf8441d1b197def2fde4d1a5d3dd98111
Tom Milliman <thomas.milliman@unh.edu>
2015-12-15 17:03:45 -0500

    added some global constants

--- pydhdt/__init__.py
+++ pydhdt/__init__.py
@@ -1,5 +1,14 @@
-"""pydhdt - ""package for accessing the UNH laser altimetry database tables""""
+"""
+pydhdt - package for accessing the UNH laser altimetry database tables
+"""
 
 __version__ = '0.1.0'
 __author__ = 'Tom Milliman <thomas.milliman@unh.edu>'
 __all__ = []
+
+# set some global vars
+DBNAME = 'icedb'
+DBHOST = 'icebridge.sr.unh.edu'
+DBUSER = 'nobody'
+DBPORT = '5432'
+EPSG_CODE = 3413
