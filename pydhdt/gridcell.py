c2acbe7e42538fc2c8e06ed4512c64131e7668c2
Tom Milliman <thomas.milliman@unh.edu>
2016-01-11 17:38:35 -0500

    fixed polygon creation

--- pydhdt/gridcell.py
+++ pydhdt/gridcell.py
@@ -13,24 +13,27 @@ def gridcell2poly(cellcntr, cellsize):
 
     Here's an example WKT Polygon string:
 
-    ST_Polygon(
+    ST_MakePolygon(
       ST_GeomFromText(
-      'LINESTRING(75.15 29.53,77 29,77.6 29.5, 75.15 29.53)'),
-      4326);
+      'LINESTRING(75.15 29.53,77 29,77.6 29.5, 75.15 29.53)',
+      4326));
 
     (I've added line breaks to make this readable.)
     """
     xcntr, ycntr = cellcntr
     deltax, deltay = cellsize
 
-    points = [[xcntr-deltax, ycntr+deltay], [xcntr+deltax, ycntr+deltay],
-              [xcntr+deltax, ycntr-deltay], [xcntr-deltax, ycntr-deltay],
+    points = [[xcntr-deltax, ycntr+deltay],
+              [xcntr+deltax, ycntr+deltay],
+              [xcntr+deltax, ycntr-deltay],
+              [xcntr-deltax, ycntr-deltay],
               [xcntr-deltax, ycntr+deltay]]
 
     coordlist = ["{0} {1}".format(x[0], x[1]) for x in points]
     coordstr = ",".join(coordlist)
-    wktstr = "ST_Polygon(ST_GeomFromText('LINESTRING({0}),{1})".format(
-        coordstr, EPSG_CODE)
+    polygeom = "ST_MakePolygon(ST_GeomFromText" + \
+               "('LINESTRING({0})',{1}))".format( coordstr,
+                                                  EPSG_CODE)
 
-    return wktstr
+    return polygeom
 
